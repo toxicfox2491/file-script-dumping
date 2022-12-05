ab15d6cdc6b1b2694cde1db575e917b5680ba960
toxicfox2491 <69102209+toxicfox2491@users.noreply.github.com>
2022-12-05 14:09:24 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/toxicfox2491/file-script-dumping/main/source/source.lua"))()
-local Window = Library.CreateLib("a catboys Util script", "Midnight")
+local Window = Library.CreateLib("a catboys Util script", "Synapse")
     -- MAIN
     local Main = Window:NewTab("Main")
     local MainSection = Main:NewSection("Main")
