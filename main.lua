1ef953288de318687be8bb97660eccb7b8593e18
toxicfox2491 <69102209+toxicfox2491@users.noreply.github.com>
2022-12-25 04:14:08 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -70,6 +70,8 @@
         lighting = game:GetService("Lighting")
         if lighting.TimeOfDay == "00:00:00" then
         lighting.TimeOfDay = 11
+        else
+       lighting.TimeOfDay = 24
         end)
 
     OtherSection:NewKeybind("quick reset", "quickly reset character", Enum.KeyCode.PageUp, function()
